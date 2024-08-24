8cad9b2fd4c520b4372e2fc4b523ac18f397dafe
Shashank <shashankdesai99@gmail.com>
2024-08-25 00:59:16 +0530

    Change string op

--- DavyWybiral/ex1.asm
+++ DavyWybiral/ex1.asm
@@ -1,13 +1,13 @@
 global _start
 section .data
-    msg db "Hello World!", 0x0a
-    len equ $ - msg
+    data db "Shashank learns OS from osdev.wiki", 0x80
+    length equ $ - data
 section .text
 _start:
     mov eax, 4
     mov ebx, 1
-    mov ecx, msg
-    mov edx, len
+    mov ecx, data
+    mov edx, length
     int 0x80
     mov eax, 1
     mov ebx, 0
